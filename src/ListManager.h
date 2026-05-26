b70f5abeac55704b5fdcf1a570b5d8443afcbd24
Semyazi <evan.simanovskis@gmail.com>
2026-05-26 16:06:53 -0400

    new API parsing

--- src/ListManager.h
+++ src/ListManager.h
@@ -11,20 +11,6 @@ class ListManager {
         inline static bool firstTimeOpen;
         inline static int filterType;
         inline static bool isSupremeSearching;
-
-        inline static void parseRequestString(std::string str) {
-            size_t isFound = str.find("_id");
-
-            while (isFound != std::string::npos) {
-                str = str.substr(isFound + 5);
-                size_t findBracket = str.find("}");
-
-                int id = stoi(str.substr(0, findBracket));
-                demonIDList.push_back(id);
-
-                isFound = str.find("_id");
-            }
-        }
         
         inline static void parseRequestString(std::string str) {
             demonIDList.clear();
