67302560a947ad1ed8d65d1f6747386b7ae83b48
Semyazi <evan.simanovskis@gmail.com>
2026-05-26 15:28:34 -0400

    uses the new AREDL API and the new geode web thing

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using namespace geode::prelude;
 class $modify(MenuLayer) {
 
 	struct Fields {
-        EventListener<web::WebTask> m_listener;
+        TaskHolder<web::WebResponse> m_listener;
 	};
 
 	bool init() {
@@ -19,46 +19,34 @@ class $modify(MenuLayer) {
 			return true;
 		}
 
-		m_fields->m_listener.bind([] (web::WebTask::Event* e) {
-            if (web::WebResponse* res = e->getValue()) {
-                auto str = res->string().unwrapOr("Failed.");
-				if (res->code() != 200 || str == "Failed." || str == "-1") {
-					ListManager::firstTimeOpen = true;
-					ListManager::filterType = -2;
-					std::string errorStr = "\n\n<cr>Could not load data from AREDL.</c>\nThe API could be down, but chances are, your internet just sucks.\n\n<cg>Restart your game to try again.</c>\n\n<cb>-Grandpa Demon</c>";
-					FLAlertLayer::create("What the??", str + errorStr, "OK")->show();
-					return;
-				}
+		log::info("Fetching AREDL Data...");
+        auto req = web::WebRequest();
 
-				ListManager::parseRequestString(str);
-		 		ListManager::firstTimeOpen = true;
-		 		ListManager::filterType = -1;
-			
-			}
-        });
+        m_fields->m_listener.spawn(
+            "Fetch AREDL Data", 
+            req.get("https://api.aredl.net/v2/api/aredl/levels?exclude_legacy=false"),
+            [](web::WebResponse res) {
+                log::info("Receieved AREDL Data");
 
-		auto req = web::WebRequest();
-		m_fields->m_listener.setFilter(req.get("https://api.aredl.net/api/aredl/list"));
+                auto str = res.string().unwrapOr("Failed.");
+                
+                if (!res.ok() || str == "Failed." || str == "-1") {
+                    ListManager::firstTimeOpen = true;
+                    ListManager::filterType = -2;
+                    if(auto scene = CCDirector::get()->getRunningScene()){
+                        std::string errorStr = "\n\n<cr>Could not load data from AREDL.</c>\nThe API could be down, but chances are, your internet just sucks.\n\n<cg>Restart your game to try again.</c>\n\n<cb>-Grandpa Demon</c>";
+                        FLAlertLayer::create("What the??", str + errorStr, "OK")->show();
+                    }
+                    return;
+                }
 
-		// if (!ListManager::firstTimeOpen) {
-		// 	web::AsyncWebRequest()
-		// 	.fetch("https://api.aredl.net/api/aredl/list")
-		// 	.text()
-		// 	.then([](std::string const& str) {
-		// 		ListManager::parseRequestString(str);
-		// 		ListManager::firstTimeOpen = true;
-		// 		ListManager::filterType = -1;
-		// 	})
-		// 	.expect([](std::string const& error) {
-		// 		ListManager::firstTimeOpen = true;
-		// 		ListManager::filterType = -2;
-		// 		std::string errorStr = "\n\n<cr>Could not load data from AREDL.</c>\nThe API could be down, but chances are, your internet just sucks.\n\n<cg>Restart your game to try again.</c>\n\n<cb>-Grandpa Demon</c>";
-		// 		FLAlertLayer::create("What the??", error + errorStr, "OK")->show();
-		// 	});
-		// }
-		
+                ListManager::parseRequestString(str);
+                ListManager::firstTimeOpen = true;
+                ListManager::filterType = -1;
+            }
+        );
 
-		return true;
+        return true;
 	}
 };
 
