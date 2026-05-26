71e751bedcb5c7dae41bd2e0671494f9e1260ba5
Semyazi <evan.simanovskis@gmail.com>
2026-05-26 17:42:49 -0400

    refactored to fix an issue where a cell gets reused for a non top-500 demon (just purges everything at the beginning to be safe)

--- src/LevelCell.cpp
+++ src/LevelCell.cpp
@@ -6,30 +6,26 @@
 using namespace geode::prelude;
 
 class $modify(LevelCell) {
-
     // use m_mainLayer as parent
     void loadCustomLevelCell() {
-        LevelCell::loadCustomLevelCell();
+        LevelCell::loadCustomLevelCell();;
 
-        if (ListManager::demonIDList.size() == 0) {
-            return;
-        }
-
-        if (m_level->m_stars != 10) {
-            return;
-        }
-
-        int aredlPos = ListManager::getPositionOfID(m_level->m_levelID);
-        if (aredlPos == -1 || aredlPos > 499) {
-            return;
-        }
-
+        // find everything
+        CCNode* layer = m_mainLayer->getChildByID("grd-demon-icon-layer");
         CCSprite* originalIcon = nullptr;
 
-        for (auto obj : CCArrayExt<CCObject*>(m_mainLayer->getChildren()))
+        if (layer) {
+            for (auto obj : CCArrayExt<CCObject*>(layer->getChildren()))
+                if (CCSprite* spr = dynamic_cast<CCSprite*>(obj))
+                    if (spr->getZOrder() == 3) {
+                        originalIcon = spr;
+                        break;
+                    }
+        } else for (auto obj : CCArrayExt<CCObject*>(m_mainLayer->getChildren()))
             if (CCNode* newObj = dynamic_cast<CCNode*>(obj))
                 if (newObj->getZOrder() == 2) {
                     newObj->setID("grd-demon-icon-layer");
+                    layer = newObj;
                     for (auto obj2 : CCArrayExt<CCObject*>(newObj->getChildren()))
                         if (CCSprite* newObj2 = dynamic_cast<CCSprite*>(obj2))
                             if (newObj2->getZOrder() == 3) {
@@ -37,41 +33,60 @@ class $modify(LevelCell) {
                                 break;
                             }
                 }
+        
+        // clean up the modded stuff just in case this level cell got recycled to a different level
+        if (layer) {
+            if (auto oldCustomIcon = layer->getChildByID("grd-custom-icon"))
+                oldCustomIcon->removeFromParentAndCleanup(true);
+            if (auto oldInfinity = layer->getChildByID("grd-infinity"))
+                oldInfinity->removeFromParentAndCleanup(true);
 
-        if (originalIcon == nullptr) {
-            return;
+            std::vector<CCNode*> children;
+            for (auto obj : CCArrayExt<CCObject*>(layer->getChildren()))
+                if (CCNode* node = dynamic_cast<CCNode*>(obj))
+                    if (node->getTag() == 133769420) children.push_back(node);
+            for (auto node : children) node->removeFromParentAndCleanup(true);
         }
+        if (originalIcon){
+            std::vector<CCSprite*>children;
+            for (auto clearObj : CCArrayExt<CCObject*>(originalIcon->getChildren()))
+                if (CCSprite* newObj = dynamic_cast<CCSprite*>(clearObj))
+                    if (newObj->getTag() == 133769420)
+                        children.push_back(newObj);
+            for (auto newObj : children)
+                newObj->removeFromParentAndCleanup(true);
+
+            originalIcon->setVisible(true);
+        }
+        
+        // add in the mod stuff (if possible)
+        if (ListManager::demonIDList.size() == 0 || m_level->m_stars != 10) return;
+        if (originalIcon == nullptr || layer == nullptr) return;
+
+        int aredlPos = ListManager::getPositionOfID(m_level->m_levelID);
+        if (aredlPos == -1 || aredlPos > 499) return;
 
         CCSprite* newIcon = ListManager::getSpriteFromPosition(aredlPos, false);
-        //CCSprite* newIcon = CCSprite::createWithSpriteFrameName("GrD_demon0.png"_spr);
-        auto layer = m_mainLayer->getChildByID("grd-demon-icon-layer");
+        if (!newIcon) return;
+        newIcon->setID("grd-custom-icon");
 
         auto newPos = originalIcon->getPosition();
-        newIcon->setPosition(originalIcon->getPosition());
-        newIcon->setZOrder(originalIcon->getZOrder()+25);
-        
-        std::vector<CCSprite*>nodesDel;
-        for (auto clearObj : CCArrayExt<CCObject*>(originalIcon->getChildren()))
-            if (CCSprite* newObj = dynamic_cast<CCSprite*>(clearObj))
-                if (newObj->getTag() == 133769420) nodesDel.push_back(newObj);
-        for(auto newObj:nodesDel) newObj->removeFromParentAndCleanup(true);
+        newIcon->setPosition(newPos);
+        newIcon->setZOrder(originalIcon->getZOrder() + 25);
 
-        std::vector<CCNode*>children;
+        std::vector<CCNode*> children;
         for (auto iconObj : CCArrayExt<CCObject*>(originalIcon->getChildren()))
             if (CCSprite* newObj = dynamic_cast<CCSprite*>(iconObj))
                 children.push_back(newObj);
-        for(auto newObj:children){
+        for (auto newObj : children) {
             newObj->setTag(133769420);
             layer->addChild(newObj);
             newObj->setPosition(newPos);
         }
 
         originalIcon->setVisible(false);
-
         layer->addChild(newIcon);
 
-        if (aredlPos <= 24) {
-            EffectsManager::addInfinitySymbol(newIcon->getPosition(), layer, aredlPos);
-        }
+        if (aredlPos <= 24) EffectsManager::addInfinitySymbol(newIcon->getPosition(), layer, aredlPos);
     }
 };
